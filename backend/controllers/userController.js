7f8bc55fb7fdba14e1f817a273d350769dca6ce6
Shaker Kattoula <smkattoula@gmail.com>
2021-05-09 14:42:04 -0700

    create register user api and encrypt password via bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
-import e from "express";
+import e, { json } from "express";
 
 // Description: Authenticate user and get token
 // Route: POST /api/users/login
@@ -25,6 +25,39 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Description: Register a new user
+// Route: POST /api/users/
+// Access: Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // Description: Get logged in user profile
 // Route: GET /api/users/profile
 // Access: Private
@@ -44,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
