5e64958a898849960b0f8e6ba4302f710774f0b1
Shaker Kattoula <smkattoula@gmail.com>
2021-05-19 16:15:45 -0700

    create order controller a, create route, and create addOrderItems api to save order items to the database

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,40 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+
+// Description: Create a new order
+// Route: POST /api/orders
+// Access: Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    const order = new Order({
+      user: req.user._id,
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+export { addOrderItems };
