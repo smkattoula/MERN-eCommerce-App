c9a46d28aa8204222a84b22e71b620ee028925db
Shaker Kattoula <smkattoula@gmail.com>
2021-05-22 17:13:43 -0700

    create getOrderById api route in the backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,4 +37,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// Description: Get order by ID
+// Route: GET /api/orders/:id
+// Access: Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById };
