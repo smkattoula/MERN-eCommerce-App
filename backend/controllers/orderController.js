ef2c2e63a3d56cb06aa297ca1145e0f8269396bb
Shaker Kattoula <smkattoula@gmail.com>
2021-06-03 17:15:31 -0700

    create table in profile screen and get logged in users order via listMyOrders action and display users orders in the UI

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -78,4 +78,13 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+// Description: Get logged in users orders
+// Route: GET /api/orders/myorders
+// Access: Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
