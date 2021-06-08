57af309d1d2520b76cbce0b9d0aa6164c91e547f
Shaker Kattoula <smkattoula@gmail.com>
2021-06-08 13:58:35 -0700

    create user delete (admin only) API endpoint in the backend and implement the user delete functionality on the frontend in the UserListScreen component

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -115,4 +115,26 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users);
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
+// Description: Delete a user
+// Route: GET /api/users/:id
+// Access: Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: "User removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
