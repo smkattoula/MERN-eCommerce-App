a22ed6c726657f1273005903b02bb9d22f4587b1
Shaker Kattoula <smkattoula@gmail.com>
2021-06-09 11:24:12 -0700

    create getUserById and updateUser API Endpoints in the backend (admin only)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,8 +84,8 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
-    (user.name = req.body.name || user.name),
-      (user.email = req.body.email || user.email);
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
 
     if (req.body.password) {
       user.password = req.body.password;
@@ -130,6 +130,45 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Description: Get a user by ID
+// Route: GET /api/users/:id
+// Access: Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// Description: Update user
+// Route: PUT /api/users/:id
+// Access: Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -137,4 +176,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
